8d1eb18c17fdc213b26135c7083d69e29418ec21
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2017-01-21 23:41:45 -1000

    renderer

---
+++ render.js
@@ -0,0 +1,101 @@
+var regl = require('regl')()
+var resl = require('resl')
+var glsl = require('glslify')
+var proj = require('glsl-proj4')
+var mat4 = require('gl-mat4')
+var camera = require('glsl-proj4-camera')(`
+  +proj=tmerc +lat_0=18.83333333333333 +lon_0=-155.5 +ellps=GRS80 +units=m
+  +k_0=0.0000019268500651226404 +x_0=0.35589838 +y_0=-0.34185734
+`.trim())
+
+resl({
+  manifest: {
+    land: {
+      type: 'text',
+      src: 'hawaii.json',
+      parser: JSON.parse
+    },
+    lines: {
+      type: 'text',
+      src: '6.json',
+      parser: JSON.parse
+    }
+  },
+  onDone: ready
+})
+
+function ready (assets) {
+  var draw = {
+    lines: lines(regl, assets.lines),
+    land: land(regl, assets.land)
+  }
+  regl.frame(function () {
+    regl.clear({ color: [0,0,0,1], depth: true })
+    draw.land()
+    draw.lines()
+  })
+}
+
+function land (regl, mesh) {
+  return regl({
+    frag: `
+      precision mediump float;
+      void main () {
+        gl_FragColor = vec4(0.1,0.1,0.1,1);
+      }
+    `,
+    vert: glsl`
+      precision mediump float;
+      #pragma glslify: forward = require('glsl-proj4/tmerc/forward')
+      #pragma glslify: proj_t = require('glsl-proj4/tmerc/t')
+      uniform proj_t proj;
+      attribute vec2 position;
+      uniform float aspect;
+      void main () {
+        vec3 p = forward(proj,position)*vec3(1,aspect,1);
+        gl_Position = vec4(p,1);
+      }
+    `,
+    uniforms: Object.assign(camera.members('proj'), {
+      aspect: function (context) {
+        return context.viewportWidth / context.viewportHeight
+      }
+    }),
+    attributes: {
+      position: mesh.positions
+    },
+    elements: mesh.cells,
+    depth: { enable: false, mask: false }
+  })
+}
+function lines (regl, mesh) {
+  return regl({
+    frag: `
+      precision mediump float;
+      void main () {
+        gl_FragColor = vec4(1,0.5,0.5,1);
+      }
+    `,
+    vert: glsl`
+      precision mediump float;
+      #pragma glslify: forward = require('glsl-proj4/tmerc/forward')
+      #pragma glslify: proj_t = require('glsl-proj4/tmerc/t')
+      uniform proj_t proj;
+      attribute vec2 position;
+      uniform float aspect;
+      void main () {
+        vec3 p = forward(proj,position)*vec3(1,aspect,1);
+        gl_Position = vec4(p,1);
+      }
+    `,
+    uniforms: Object.assign(camera.members('proj'), {
+      aspect: function (context) {
+        return context.viewportWidth / context.viewportHeight
+      }
+    }),
+    attributes: {
+      position: mesh.positions
+    },
+    elements: mesh.cells
+  })
+}
