9ea9476545ef0c5b009156c3d7d2c5c227c8f6f6
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2017-01-24 10:43:59 -1000

    double up points, angles

--- render.js
+++ render.js
@@ -3,10 +3,14 @@ var resl = require('resl')
 var glsl = require('glslify')
 var proj = require('glsl-proj4')
 var mat4 = require('gl-mat4')
-var camera = require('glsl-proj4-camera')(`
+var camera = require('glsl-proj4-camera')(location.hash.replace(/^#/,'') || `
   +proj=tmerc +lat_0=18.83333333333333 +lon_0=-155.5 +ellps=GRS80 +units=m
   +k_0=0.0000019268500651226404 +x_0=0.35589838 +y_0=-0.34185734
 `.trim())
+
+camera.on('update', function () {
+  location.hash = camera.string()
+})
 
 resl({
   manifest: {
@@ -82,10 +86,15 @@ function lines (regl, mesh) {
       #pragma glslify: proj_t = require('glsl-proj4/tmerc/t')
       uniform proj_t proj;
       attribute vec2 position;
+      attribute float angle;
       uniform float aspect;
       void main () {
         vec3 p = forward(proj,position)*vec3(1,aspect,1);
-        gl_Position = vec4(p,1);
+        vec2 adir = vec2(cos(angle),sin(angle))*0.001;
+        vec3 np = forward(proj,position+adir)*vec3(1,aspect,1);
+        vec3 dir = normalize(np-p);
+        gl_Position = vec4(p+dir*0.002,1);
+        //gl_Position = vec4(p,1);
       }
     `,
     uniforms: Object.assign(camera.members('proj'), {
@@ -94,8 +103,13 @@ function lines (regl, mesh) {
       }
     }),
     attributes: {
-      position: mesh.positions
+      position: mesh.positions,
+      angle: mesh.angles
     },
-    elements: mesh.cells
+    elements: mesh.cells,
+    blend: {
+      enable: true,
+      func: { src: 'src alpha', dst: 'one minus src alpha' }
+    }
   })
 }
--- tomesh.js
+++ tomesh.js
@@ -1,23 +1,32 @@
 var parser = require('osm-pbf-parser')
+var defined = require('defined')
 var through = require('through2')
-var docs = {}, mesh = { positions: [], cells: [] }
+var docs = {}
+var mesh = { positions: [], cells: [], angles: [] }
 
 process.stdin.pipe(parser())
   .pipe(through.obj(write, end))
+
 function write (items, enc, next) {
   for (var i = 0; i < items.length; i++) {
     var item = items[i]
     docs[item.id] = item
     if (item.refs && item.tags.highway) {
-      var poly = []
-      var n = mesh.positions.length
-      for (var j = 0; j < item.refs.length-1; j++) {
-        mesh.cells.push([n+j,n+j+1])
-      }
       for (var j = 0; j < item.refs.length; j++) {
         var ref = item.refs[j]
         var pt = [docs[ref].lon,docs[ref].lat]
-        mesh.positions.push(pt)
+        mesh.positions.push(pt,pt)
+        var n = mesh.positions.length
+        if (j > 0) {
+          mesh.cells.push([n-1,n-3])
+          mesh.cells.push([n-2,n-4])
+        }
+        var pref = defined(item.refs[j-1],ref)
+        var ppt = [docs[pref].lon,docs[pref].lat]
+        var nref = defined(item.refs[j+1],ref)
+        var npt = [docs[nref].lon,docs[nref].lat]
+        var theta = Math.atan2(ppt[1]-npt[1],ppt[0]-npt[0])
+        mesh.angles.push(theta,(theta+Math.PI)%(2*Math.PI))
       }
     }
   }
