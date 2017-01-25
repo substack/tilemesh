240d366b3c8f5292b0ef8f8633cbb81d5dd7c8ff
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2017-01-24 17:05:11 -1000

    colors based on highway type

--- render.js
+++ render.js
@@ -19,7 +19,7 @@ resl({
       src: 'hawaii.json',
       parser: JSON.parse
     },
-    lines: {
+    data: {
       type: 'text',
       src: '6.json',
       parser: JSON.parse
@@ -30,13 +30,13 @@ resl({
 
 function ready (assets) {
   var draw = {
-    lines: lines(regl, assets.lines),
+    highway: highway(regl, assets.data.highway),
     land: land(regl, assets.land)
   }
   regl.frame(function () {
     regl.clear({ color: [0,0,0,1], depth: true })
     draw.land()
-    draw.lines()
+    draw.highway()
   })
 }
 
@@ -72,12 +72,13 @@ function land (regl, mesh) {
     depth: { enable: false, mask: false }
   })
 }
-function lines (regl, mesh) {
+function highway (regl, mesh) {
   return regl({
     frag: `
       precision mediump float;
+      varying vec3 vcolor;
       void main () {
-        gl_FragColor = vec4(1,0.5,0.5,1);
+        gl_FragColor = vec4(vcolor,1);
       }
     `,
     vert: glsl`
@@ -86,24 +87,51 @@ function lines (regl, mesh) {
       #pragma glslify: proj_t = require('glsl-proj4/tmerc/t')
       uniform proj_t proj;
       attribute vec2 position;
-      attribute float angle;
+      attribute float angle, roadtype;
       uniform float aspect;
+      uniform vec3 colors[19];
+      varying vec3 vcolor;
       void main () {
+        vcolor = colors[int(roadtype)];
         vec3 p = forward(proj,position)*vec3(1,aspect,1);
         vec2 adir = vec2(cos(angle),sin(angle))*0.001;
         vec3 np = forward(proj,position+adir)*vec3(1,aspect,1);
         vec3 dir = normalize(np-p);
-        gl_Position = vec4(p+dir*proj.k0*20.0,1);
+        gl_Position = vec4((p+dir
+          * max(0.005*pow(1.0/roadtype,1.2),proj.k0*(40.0-roadtype*2.0))
+            * step(1e-6,proj.k0/roadtype)).xy,
+          1.0-1.0/roadtype,
+          1);
       }
     `,
     uniforms: Object.assign(camera.members('proj'), {
       aspect: function (context) {
         return context.viewportWidth / context.viewportHeight
-      }
+      },
+      'colors[0]': [1,0,0],
+      'colors[1]': [1,0.8,0],
+      'colors[2]': [1,0.6,0],
+      'colors[3]': [1,0.4,0.3],
+      'colors[4]': [1,0.3,0.5],
+      'colors[5]': [0.5,0.5,0.5],
+      'colors[6]': [0.5,0.5,0.5],
+      'colors[7]': [0.5,0.5,0.5],
+      'colors[8]': [0.5,0.5,0.5],
+      'colors[9]': [0.5,0.5,0.5],
+      'colors[10]': [0.5,0.5,0.5],
+      'colors[11]': [0.5,0.5,0.5],
+      'colors[12]': [0.5,0.5,0.5],
+      'colors[13]': [0.5,0.5,0.5],
+      'colors[14]': [0.5,0.5,0.5],
+      'colors[15]': [0.5,0.5,0.5],
+      'colors[16]': [0.5,0.5,0.5],
+      'colors[17]': [0.5,0.5,0.5],
+      'colors[18]': [0.5,0.5,0.5]
     }),
     attributes: {
       position: mesh.positions,
-      angle: mesh.angles
+      angle: mesh.angles,
+      roadtype: mesh.types
     },
     elements: mesh.cells,
     blend: {
--- tomesh.js
+++ tomesh.js
@@ -2,7 +2,31 @@ var parser = require('osm-pbf-parser')
 var defined = require('defined')
 var through = require('through2')
 var docs = {}
-var mesh = { positions: [], cells: [], angles: [] }
+var mesh = {
+  highway: { positions: [], cells: [], angles: [], types: [] }
+}
+
+var highway = {
+  motorway: 1,
+  trunk: 2,
+  primary: 3,
+  secondary: 4,
+  tertiary: 5,
+  unclassified: 6,
+  residential: 7,
+  service: 8,
+  living_street: 9,
+  pedestrian: 10,
+  track: 11,
+  bus_guideway: 12,
+  escape: 13,
+  raceway: 14,
+  road: 15,
+  footway: 16,
+  bridleway: 17,
+  steps: 18,
+  path: 19
+}
 
 process.stdin.pipe(parser())
   .pipe(through.obj(write, end))
@@ -15,19 +39,25 @@ function write (items, enc, next) {
       for (var j = 0; j < item.refs.length; j++) {
         var ref = item.refs[j]
         var pt = [docs[ref].lon,docs[ref].lat]
-        mesh.positions.push(pt,pt)
-        var n = mesh.positions.length
+        mesh.highway.positions.push(pt,pt)
+        var n = mesh.highway.positions.length
         if (j > 0) {
-          mesh.cells.push([n-1,n-2,n-3])
-          mesh.cells.push([n-2,n-4,n-3])
+          mesh.highway.cells.push([n-1,n-2,n-3])
+          mesh.highway.cells.push([n-2,n-4,n-3])
         }
         var pref = defined(item.refs[j-1],ref)
         var ppt = [docs[pref].lon,docs[pref].lat]
         var nref = defined(item.refs[j+1],ref)
         var npt = [docs[nref].lon,docs[nref].lat]
         var theta = Math.atan2(npt[1]-ppt[1],npt[0]-ppt[0])
-        mesh.angles.push(angle(theta+Math.PI/2))
-        mesh.angles.push(angle(theta-Math.PI/2))
+        mesh.highway.angles.push(angle(theta+Math.PI/2))
+        mesh.highway.angles.push(angle(theta-Math.PI/2))
+        var roadtype = defined(
+          highway[item.tags.highway],
+          highway[item.tags.highway+'_link'],
+          highway.road
+        )
+        mesh.highway.types.push(roadtype,roadtype)
       }
     }
   }
