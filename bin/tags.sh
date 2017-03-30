82b36fd6fed8b4a17dd45dc6bf14fe57c67da974
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2017-03-29 20:34:37 -1000

    script to scrape a list of map features from the osm wiki

---
+++ bin/tags.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+load () {
+  curl -sS "http://wiki.openstreetmap.org/w/api.php?titles=$1&action=query\
+&prop=revisions&rvprop=content&format=json" \
+    | jsonstream query.pages.*.revisions.0 | json 0.\*
+}
+
+load Map_Features \
+  | sed '0,/^== Primary features/ d; 0,/^$/ {/^{{Map_Features:/ !d
+      s/^{{Map_Features://; s/}}$// p}; d' \
+  | while read page; do
+    load "Template:Map_Features:$page" \
+      | grep "Tag:$page=" | sed 's/.*Tag://; s/}}.*//'
+  done \
+  | sed 's/=/,/; s/\]\].*'
