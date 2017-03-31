4d4c6d5fba93234a699a52630caf341a063feec5
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2017-03-30 23:33:23 -1000

    other category for feature json and enum

--- build/feature-enum.js
+++ build/feature-enum.js
@@ -7,17 +7,22 @@ process.stdin.pipe(csv())
   .pipe(process.stdout)
 
 function enumc () {
-  var n = 0
+  var n = 0, cat = null
   var stream = through.obj(write, end)
   stream.push('const enum features {')
   return stream
   function write (row, enc, next) {
+    if (cat && cat !== row.category) {
+      this.push(',\n' + uc(cat) + '_OTHER = ' + (n++))
+    }
+    cat = row.category
     var k = uc(row.category + '_' + row.type)
     var v = String(n++)
     next(null, (n > 1 ? ',\n' : '\n') + k + '=' + v)
   }
   function end (next) {
-    this.push('\n};\n#pragma glslify: export(features)\n')
+    this.push(',\n' + uc(cat) + '_OTHER = ' + (n++)
+      + '\n};\n#pragma glslify: export(features)\n')
     next()
   }
 }
--- build/feature-json.js
+++ build/feature-json.js
@@ -7,17 +7,21 @@ process.stdin.pipe(csv())
   .pipe(process.stdout)
 
 function enumjson () {
-  var n = 0
+  var n = 0, cat = null
   var stream = through.obj(write, end)
   stream.push('{')
   return stream
   function write (row, enc, next) {
+    if (cat && cat !== row.category) {
+      this.push(',\n' + JSON.stringify(cat + '.other') + ':' + (n++))
+    }
+    cat = row.category
     var k = JSON.stringify(row.category + '.' + row.type)
     var v = JSON.stringify(n++)
     next(null, (n > 1 ? ',\n' : '') + k + ':' + v)
   }
   function end (next) {
-    this.push('}\n')
+    this.push(',\n' + JSON.stringify(cat + '.other') + ':' + (n++) + '}\n')
     next()
   }
 }
