f93594fe5551e3ad3e30f30e8a03d8b68754dc85
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2017-04-19 21:09:28 -1000

    write out point data

--- tilemesh.cpp
+++ tilemesh.cpp
@@ -4,18 +4,17 @@
 int main (int argc, char **argv) {
   char *data = (char*) malloc(4096);
   size_t len;
-  tilemesh::o5mWriter tw;
+  tilemesh::o5m tm;
   do {
     len = fread(data, sizeof(char), 4096, stdin);
     try {
-      tw.write(data, len);
+      tm.write(data, len);
     } catch (char *err) {
       printf("error: %s\n", err);
     }
   } while (len == 4096);
-  while (tw.read(data, 4096, &len)) {
+  while (tm.read(data, 4096, &len)) {
     fwrite(data, sizeof(char), len, stdout);
   }
-  printf("areas: %d\n", tw.tdata.areas.length);
   return 0;
 }
--- tilemesh.h
+++ tilemesh.h
@@ -5,11 +5,12 @@
 #include <map>
 #include <o5mdecoder.h>
 #include <tilemesh_features.h>
+#include <endian.h>
 
 namespace tilemesh {
 
-const char magic[10] = "TILEMESH\n";
-const char version[256] = "1.0.0\n";
+const char magic[11] = "TILEMESH\n\0";
+const char version[256] = "1.0.0\n\0";
 static char tmp[256];
 template<class T> struct ListItem {
   T *data;
@@ -47,6 +48,19 @@ struct Point {
     type = _type;
     lon = _lon;
     lat = _lat;
+  }
+  size_t write (char *dst) {
+    size_t pos = 0;
+    *(uint64_t*)(dst+pos) = htobe64(id);
+    pos += sizeof(uint64_t);
+    *(uint16_t*)(dst+pos) = htobe16(type);
+    pos += sizeof(uint16_t);
+    // assumes integer byte order is the same as floating point byte order
+    *(uint32_t*)(dst+pos) = htobe32(*((uint32_t*)&lon));
+    pos += sizeof(float);
+    *(uint32_t*)(dst+pos) = htobe32(*((uint32_t*)&lat));
+    pos += sizeof(float);
+    return pos;
   }
 };
 struct Position {
@@ -114,8 +128,8 @@ void triangulate (List<Position> *outplist, List<Cell> *outclist,
 List<Position> *inplist) {
   // TODO
 }
-size_t find_feature_kv (tilemesh::Features *features, char *key, char *value) {
-  tilemesh::FeaturesIterator ti;
+size_t find_feature_kv (Features *features, char *key, char *value) {
+  FeaturesIterator ti;
   size_t i,j;
   for (i = 0; key[i] != 0; i++) tmp[i] = key[i];
   tmp[i++] = '.';
@@ -138,42 +152,48 @@ size_t find_feature (Features *features, o5mdecoder::Doc *doc) {
   }
   return 0;
 }
-struct o5mWriter {
+struct o5m {
   char *data, *dbuf, *table;
   o5mdecoder::Decoder *decoder;
   uint64_t ref;
-  std::map<uint64_t,tilemesh::Position*> nodes;
-  std::map<uint64_t,tilemesh::Position*>::const_iterator ipt;
-  tilemesh::Features features;
-  tilemesh::Data tdata;
-  tilemesh::Position *pos;
-  tilemesh::Point *pt;
-  tilemesh::Area *area;
-  tilemesh::Line *line;
-  tilemesh::Outline *outline;
-  tilemesh::List<tilemesh::Position> *poslist;
-  tilemesh::List<tilemesh::Position> *aplist;
-  tilemesh::List<tilemesh::Cell> *aclist;
-  o5mWriter () {
+  std::map<uint64_t,Position*> nodes;
+  std::map<uint64_t,Position*>::const_iterator ipt;
+  Features features;
+  Data tdata;
+  Position *pos;
+  Point *pt;
+  Area *area;
+  Line *line;
+  Outline *outline;
+  List<Position> *poslist;
+  List<Position> *aplist;
+  List<Cell> *aclist;
+  ListItem<Point> *ipoint;
+
+  size_t readpos, readnum;
+  uint16_t readstage;
+  o5m () {
     dbuf = (char*) malloc(16384);
     table = (char*) malloc(256*15000);
     decoder = new o5mdecoder::Decoder(dbuf,16384,table);
     load_features(&features);
+    readpos = 0;
+    readstage = 0;
   }
   void write (char *data, size_t len) {
     size_t ftype;
     decoder->write(data, len);
     while (decoder->read()) {
       if (decoder->node) {
-        pt = new tilemesh::Point(decoder->node->id,
-          tilemesh::find_feature(&features, decoder->node),
+        pt = new Point(decoder->node->id,
+          find_feature(&features, decoder->node),
           decoder->node->lon, decoder->node->lat);
         tdata.points.push(pt);
-        pos = new tilemesh::Position(decoder->node->lon, decoder->node->lat);
+        pos = new Position(decoder->node->lon, decoder->node->lat);
         nodes[decoder->node->id] = pos;
       } else if (decoder->way) {
-        ftype = tilemesh::find_feature(&features, decoder->way);
-        poslist = new tilemesh::List<tilemesh::Position>;
+        ftype = find_feature(&features, decoder->way);
+        poslist = new List<Position>;
         while (decoder->way->getRef(&ref)) {
           ipt = nodes.find(ref);
           if (ipt == nodes.end()) continue;
@@ -181,22 +201,62 @@ struct o5mWriter {
         }
         if (poslist->length >= 2
         && poslist->first->data == poslist->last->data) { // closed, area
-          outline = new tilemesh::Outline(decoder->way->id, ftype, poslist);
+          outline = new Outline(decoder->way->id, ftype, poslist);
           tdata.outlines.push(outline);
-          aplist = new tilemesh::List<tilemesh::Position>;
-          aclist = new tilemesh::List<tilemesh::Cell>;
-          tilemesh::triangulate(aplist,aclist,poslist);
-          area = new tilemesh::Area(decoder->way->id, ftype, aplist, aclist);
+          aplist = new List<Position>;
+          aclist = new List<Cell>;
+          triangulate(aplist,aclist,poslist);
+          area = new Area(decoder->way->id, ftype, aplist, aclist);
           tdata.areas.push(area);
         } else { // open, line
-          line = new tilemesh::Line(decoder->way->id, ftype, poslist);
+          line = new Line(decoder->way->id, ftype, poslist);
           tdata.lines.push(line);
         }
       }
     }
   }
   bool read (char *data, size_t len, size_t *outlen) {
-    return false;
+    size_t n, pos = 0;
+    *outlen = 0;
+    while (true) {
+      switch (readstage) {
+        case 0:
+          n = strlen(magic);
+          if (len-pos < n) return true;
+          strncpy(data+pos, magic, n);
+          *outlen = (pos += n);
+          readstage++;
+          break;
+        case 1:
+          n = strlen(version);
+          if (len-pos < n) return true;
+          strncpy(data+pos, version, n);
+          *outlen = (pos += n);
+          readstage++;
+          break;
+        case 2: // point length
+          if (len-pos < sizeof(uint32_t)) return true;
+          *(uint32_t*)(data+pos) = htobe32((uint32_t) tdata.points.length);
+          *outlen = (pos += sizeof(uint32_t));
+          readstage++;
+          ipoint = tdata.points.first;
+          //memcpy(data+pos,(char*) &(ipoint->data), sizeof(Point));
+          //*outlen = (pos += sizeof(Point));
+          break;
+        case 3: // point data
+          if (ipoint == NULL) {
+            readstage++;
+            continue;
+          }
+          if (len-pos < sizeof(Point)) return true;
+          *outlen = (pos += ipoint->data->write(data+pos));
+          ipoint = ipoint->next;
+          break;
+        default:
+          return false;
+          break;
+      }
+    }
   }
 };
 };
